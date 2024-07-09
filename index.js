a461abc6ba6570889c8303759dae18c82dc88ef4
Pirzada Abdur Rehman <138493046+pirzada03@users.noreply.github.com>
2024-07-09 18:39:56 +0500

    added test and build

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello DEVOPS!'));
-app.listen(3000, () => console.log('Server ready'));
+app.listen(3000, () => console.log('Our app is up and running. Server ready'));
+
+module.exports = app;
